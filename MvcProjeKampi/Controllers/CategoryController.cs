8d4a44d8a2b36219f40d829bb664f22332449ed9
serpil-sarioglu <serpilsarioglu81@gmail.com>
2022-06-03 20:59:53 +0300

    Updated project files.

--- MvcProjeKampi/Controllers/CategoryController.cs
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -1,16 +1,58 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FluentValidation.Results;
 
 namespace MvcProjeKampi.Controllers
 {
     public class CategoryController : Controller
     {
         // GET: Category
+        //CategoryManager cm = new CategoryManager();
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
+            return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            //var categoryvalues = cm.GetAllBL();
+            //return View(categoryvalues);
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            //cm.CategoryAddBL(p);
+            //return RedirectToAction("GetCategoryList");
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate(p);
+            if(results.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("GetCategoryList");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
     }
---
+++ MvcProjeKampi/Controllers/IstatistikController.cs
@@ -0,0 +1,30 @@
+﻿using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class IstatistikController : Controller
+    {
+        Context c = new Context();
+        // GET: Istatistik
+        public ActionResult Index()
+        {
+            var result1 = c.Categories.Count();
+            var result2 = c.Headings.Count(x => x.Category.CategoryID == 8);
+            var result3 = c.Writers.Count(x => x.WriterName.Contains("a"));
+            var result4 = c.Headings.Max(x => x.Category.CategoryName);
+            var result5 = c.Categories.Count(x => x.CategoryStatus == true);
+            var result6 = c.Categories.Count(x => x.CategoryStatus == false);
+            ViewBag.CategoryCount = result1;
+            ViewBag.HeadingCountCategoryID = result2;
+            ViewBag.WriterCountCharA = result3;
+            ViewBag.MaxHeadingCategoryName = result4;
+            ViewBag.DiffCategoryStatus = result5 - result6;
+            return View();
+        }
+    }
+}
